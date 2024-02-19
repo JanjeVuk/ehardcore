ec823b29bf2360815aba92888085068ab9cf792d
janjevuk <janjevuk@outlook.be>
2024-02-19 21:20:31 +0100

    Add documentation to CommandMenu class
    The CommandMenu class and its onCommand method are now fully documented. This includes detailed descriptions on its purpose, actions, and parameter annotations. The commenting style is consistent with Java's standard and improves code readability while explaining the functionality of the command.

--- src/main/java/be/janjevuk/ehardcore/player/command/CommandMenu.java
+++ src/main/java/be/janjevuk/ehardcore/player/command/CommandMenu.java
@@ -6,11 +6,26 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * CommandMenu class that handles the menu command in the game
+ */
 public class CommandMenu implements CommandExecutor {
 
+    /**
+     * This method is called whenever a player uses the menu command.
+     * If the sender isn't a player or the player doesn't have permission, this method does nothing.
+     * If the player has permission, this method opens the main gui.
+     *
+     * @param sender    The sender of the command, should be a player
+     * @param command   The command that was used
+     * @param label     An alias of the command that was used. This can contain spaces if the command consisted of multiple words
+     * @param args      Any additional arguments passed with the command. This usually comes after the command label.
+     * @return          Returns true if the command was successfully processed, otherwise false.
+     */
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
 
         if(!(sender instanceof Player)){
             Bukkit.getLogger().fine("You're not a player");
