bae0785cde0447726ae401a77d40fd2b31016ffb
Alexandre <janjevuk@outlook.be>
2024-02-19 16:58:02 +0100

    Add Item handling and interaction to GuiBuilder
    The implementation of item handling methods and interaction security checks have been added in the GuiBuilder class. These new features allow items to be added to a menu, sounds played upon interactions, and events secured to only specific user interactions.

--- src/main/java/be/janjevuk/ehardcore/gui/builder/GuiBuilder.java
+++ src/main/java/be/janjevuk/ehardcore/gui/builder/GuiBuilder.java
@@ -3,7 +3,10 @@ package be.janjevuk.ehardcore.gui.builder;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 
 public class GuiBuilder {
@@ -18,6 +21,10 @@ public class GuiBuilder {
         this.itemContour = new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE, 1, " ");
         this.back = new ItemBuilder(Material.SPRUCE_DOOR, 1, "§cback");
         setupTemplate();
+    }
+
+    public void addItem(ItemBuilder item, int x, int y){
+        inventory.setItem((y - 1) * 9 + x - 1, item.build());
     }
 
     private void setupTemplate() {
@@ -37,6 +44,24 @@ public class GuiBuilder {
 
         inventory.setItem(inventory.getSize() - 5, back.build());
     }
+
+
+    public static void playSound(Sound sound, Player player){
+        player.playSound(player.getLocation(), sound, 1f, 1f);
+    }
+
+    public static Boolean secureInteraction(InventoryClickEvent event, String title) {
+        if (event.getSlotType() == InventoryType.SlotType.OUTSIDE) return false;
+        if (event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR) return false;
+        // Check if target inventory has the same title
+        return event.getView().title().equals(Component.text(title));
+
+    }
+
+
+
+
+
 
     public Inventory build() {
         return this.inventory;
